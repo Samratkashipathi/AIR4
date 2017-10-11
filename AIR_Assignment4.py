8859b48d51857275562d96ad5b16dd66623d5974
Samrat <samratkashipathi1@gmail.com>
2017-10-12 00:45:33 +0530

    Building index

--- AIR_Assignment4.py
+++ AIR_Assignment4.py
@@ -1,4 +1,5 @@
 import nltk
+import string
 from nltk.stem.porter import *
 from nltk import word_tokenize
 from nltk.corpus.reader.util import *
@@ -7,35 +8,52 @@ from nltk.corpus import stopwords
 
 stemmer = PorterStemmer()
 
-stop = set(stopwords.words('english'))
+stop_words = set(stopwords.words('english'))
 # print(stop)
+def stem(word):
+	return stemmer.stem(word)
 
-def preprocesing(word):
-	if word not in stop:
-		word = word.lower()
-		return word
+def preProcessing(content):
+	tokens = nltk.wordpunct_tokenize(content)
+	tokens = list(map(lambda x : x.lower(), tokens))
+	tokens = list(filter(lambda x : x not in stop_words,tokens))
+	tokens = list(filter(lambda x : len(x)>1,tokens))
+	tokens = list(map(stem,tokens))
+	return tokens
 
-def buildInvertedIndex(doc_id , content):
-	content_array = content.split(' ')
-	# print(content_array)
-	for i in range(len(content_array)):
-		word = preprocesing(content_array[i])
-		# print(word)
+def buildIndex(index, doc_id, tokens):
+	print(tokens)
+	for word in tokens:
+		print(word)
+		if(word not in index):
+			index[word] = [{doc_id:1}]
+		else:
+			list = index[word]
+			for i in range(len(list)):
+				if(doc_id in list[i]):
+					# print(list[i])
+					
+	print(len(index))
+	return index
+
+def main():
+	# file_name = 'cran.all.1400'
+	file_name = 'test.all.1'
+
+	with open(file_name) as f:
+	    data = f.read()
+
+	doc = data.split('.I')
+	del(doc[0])
+	index = {}
+	for i in range(len(doc)):
+		doc_id = doc[i].split('.T')[0]
+		print(doc_id)
+		content = doc[i].split('.W')[1]
+		tokens = preProcessing(content)
+		index = buildIndex(index, doc_id, tokens)
+	
+	print(index['samrat'])		
 
 
-# file_name = 'cran.all.1400'
-file_name = 'test.all.10'
-with open(file_name) as f:
-    data = f.read()
-# print(data)
-doc = data.split('.I')
-del(doc[0])
-# print(doc[1])
-for i in range(len(doc)):
-	doc_id = doc[i].split('.T')[0]
-	print(doc_id)
-	content = doc[i].split('.W')[1]
-	# print(content)
-	buildInvertedIndex(doc_id,content)
-
-# print(inverted_index)
+main()
