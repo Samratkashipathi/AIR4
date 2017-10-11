2cf07b6b6b8a00fc2d7c5a41635eb20a9e1ed563
Samrat <samratkashipathi1@gmail.com>
2017-10-11 21:03:33 +0530

    First stage preprocessing and testing nltk in python

--- AIR_Assignment4.py
+++ AIR_Assignment4.py
@@ -1,3 +1,41 @@
 import nltk
 from nltk.stem.porter import *
+from nltk import word_tokenize
+from nltk.corpus.reader.util import *
+from nltk.corpus.reader.api import *
+from nltk.corpus import stopwords
+
 stemmer = PorterStemmer()
+
+stop = set(stopwords.words('english'))
+# print(stop)
+
+def preprocesing(word):
+	if word not in stop:
+		word = word.lower()
+		return word
+
+def buildInvertedIndex(doc_id , content):
+	content_array = content.split(' ')
+	# print(content_array)
+	for i in range(len(content_array)):
+		word = preprocesing(content_array[i])
+		# print(word)
+
+
+# file_name = 'cran.all.1400'
+file_name = 'test.all.10'
+with open(file_name) as f:
+    data = f.read()
+# print(data)
+doc = data.split('.I')
+del(doc[0])
+# print(doc[1])
+for i in range(len(doc)):
+	doc_id = doc[i].split('.T')[0]
+	print(doc_id)
+	content = doc[i].split('.W')[1]
+	# print(content)
+	buildInvertedIndex(doc_id,content)
+
+# print(inverted_index)
